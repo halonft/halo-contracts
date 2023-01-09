a21d525f5da71fc0ad5cc69b5e125c2bd1211738
halonft <halonft.art@gmail.com>
2023-01-09 16:34:25 +0800

    format

--- HaloItem.sol
+++ HaloItem.sol
@@ -1,29 +1,29 @@
 /***
-* MIT License
-* ===========
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*                             
-*     __  _____    __    ____ 
-*    / / / /   |  / /   / __ \
-*   / /_/ / /| | / /   / / / /
-*  / __  / ___ |/ /___/ /_/ / 
-* /_/ /_/_/  |_/_____/\____/  
-*                             
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-*/
+ * MIT License
+ * ===========
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *     __  _____    __    ____
+ *    / / / /   |  / /   / __ \
+ *   / /_/ / /| | / /   / / / /
+ *  / __  / ___ |/ /___/ /_/ /
+ * /_/ /_/_/  |_/_____/\____/
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
 
 // SPDX-License-Identifier: MIT
 
@@ -36,8 +36,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "./Interface/IAdorn1155.sol";
 
-contract HaloItem is ERC1155, Ownable,IAdorn1155 {
-
+contract HaloItem is ERC1155, Ownable, IAdorn1155 {
     using Strings for uint256;
 
     using EnumerableSet for EnumerableSet.UintSet;
@@ -46,8 +45,9 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
     string public _name;
     string public _symbol;
 
-    string public _baseURI =  "https://ipfs-v2.halonft.art/bsc/avatar/halo1155/token/";
-    string public _metatype =  ".json";
+    string public _baseURI =
+        "https://ipfs-v2.halonft.art/bsc/avatar/halo1155/token/";
+    string public _metatype = ".json";
 
     EnumerableSet.UintSet private _mintedIds;
     mapping(address => bool) public _minters;
@@ -57,11 +57,14 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
         _;
     }
 
-    constructor( string memory name,string memory symbol, string memory baseURI, string memory metatype) 
-    ERC1155(baseURI) 
-    {
-        _name=name;
-        _symbol=symbol;
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseURI,
+        string memory metatype
+    ) ERC1155(baseURI) {
+        _name = name;
+        _symbol = symbol;
         _metatype = metatype;
         _baseURI = baseURI;
 
@@ -85,15 +88,14 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
     /**
      * @dev function to set the metadata file type
      */
-    function setMetaType(string memory metatype) public onlyOwner{
+    function setMetaType(string memory metatype) public onlyOwner {
         _metatype = metatype;
     }
 
     /**
      * @dev function to set a base url of the metadata
      */
-    function setURI(string memory baseURI) public  onlyOwner
-    {
+    function setURI(string memory baseURI) public onlyOwner {
         _baseURI = baseURI;
         _setURI(baseURI);
     }
@@ -101,9 +103,23 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
     /**
      * @dev function to get the metadata url by tokenId
      */
-    function uri(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_mintedIds.contains(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return bytes(_baseURI).length > 0 ? string(abi.encodePacked(_baseURI, tokenId.toString(), _metatype)) : "";
+    function uri(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _mintedIds.contains(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return
+            bytes(_baseURI).length > 0
+                ? string(
+                    abi.encodePacked(_baseURI, tokenId.toString(), _metatype)
+                )
+                : "";
     }
 
     /**
@@ -114,10 +130,14 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
      * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
      * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received}, which is called upon a safe transfer.
      */
-    function mint(address account, uint256 tokenId, uint256 amount, bytes memory data) external override onlyMinter
-    {
+    function mint(
+        address account,
+        uint256 tokenId,
+        uint256 amount,
+        bytes memory data
+    ) external override onlyMinter {
         _mint(account, tokenId, amount, data);
-        if(!_mintedIds.contains(tokenId)){
+        if (!_mintedIds.contains(tokenId)) {
             _mintedIds.add(tokenId);
         }
     }
@@ -130,13 +150,16 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
      * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
      * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received}, which is called upon a safe transfer.
      */
-    function mintBatch(address account, uint256[] memory tokenIds, uint256[] memory amounts, bytes memory data) external override onlyMinter
-     {
+    function mintBatch(
+        address account,
+        uint256[] memory tokenIds,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external override onlyMinter {
         _mintBatch(account, tokenIds, amounts, data);
 
-        for(uint i=0; i<tokenIds.length; i++){
-            
-            if(!_mintedIds.contains(tokenIds[i])){
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            if (!_mintedIds.contains(tokenIds[i])) {
                 _mintedIds.add(tokenIds[i]);
             }
         }
@@ -147,11 +170,16 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
      * @param account The address that will burn token.
      * @param tokenId uint256 id of the ERC1155 token to be burned.
      */
-    function burn(address account, uint256 tokenId, uint256 amount) external override onlyMinter
-     {
-        require( account == msg.sender ||  isApprovedForAll(account,msg.sender), "ERC1155: burn caller is not owner nor approved");
+    function burn(
+        address account,
+        uint256 tokenId,
+        uint256 amount
+    ) external override onlyMinter {
+        require(
+            account == msg.sender || isApprovedForAll(account, msg.sender),
+            "ERC1155: burn caller is not owner nor approved"
+        );
         _burn(account, tokenId, amount);
-
     }
 
     /**
@@ -160,9 +188,15 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
      * @param tokenIds uint256 id of the ERC1155 tokens to be burned.
      * @param amounts the amount of the ERC1155 tokens to be burned.
      */
-    function burnBatch(address account, uint256[] memory tokenIds, uint256[] memory amounts) external override onlyMinter
-     {
-        require( account == msg.sender || isApprovedForAll(account,msg.sender), "ERC1155: burn caller is not owner nor approved");
+    function burnBatch(
+        address account,
+        uint256[] memory tokenIds,
+        uint256[] memory amounts
+    ) external override onlyMinter {
+        require(
+            account == msg.sender || isApprovedForAll(account, msg.sender),
+            "ERC1155: burn caller is not owner nor approved"
+        );
         _burnBatch(account, tokenIds, amounts);
     }
 
@@ -172,46 +206,53 @@ contract HaloItem is ERC1155, Ownable,IAdorn1155 {
      * @param offset page index
      * @param pageMax the max count of one page
      */
-     function tokensOfOwner(address owner, uint256 offset, uint256 pageMax ) external override view returns ( IAdorn1155.NftInfo1155[] memory nftInfos) {
+    function tokensOfOwner(
+        address owner,
+        uint256 offset,
+        uint256 pageMax
+    )
+        external
+        view
+        override
+        returns (IAdorn1155.NftInfo1155[] memory nftInfos)
+    {
+        require(pageMax > 0, "invalid page size!");
 
-        require(pageMax>0, "invalid page size!");
-        
         uint256 balance = _mintedIds.length();
         uint256 maxCount = 0;
-        if(balance <= pageMax){
+        if (balance <= pageMax) {
             maxCount = balance;
-        }
-        else{
+        } else {
             maxCount = pageMax;
-            uint256 pages = balance/pageMax;
-        
-            require(pages>=offset, "invalid page size!");
+            uint256 pages = balance / pageMax;
 
-            if(pages == offset){
-                maxCount = balance%pageMax;
+            require(pages >= offset, "invalid page size!");
+
+            if (pages == offset) {
+                maxCount = balance % pageMax;
                 require(maxCount > 0, "invalid page size!");
             }
         }
 
         nftInfos = new IAdorn1155.NftInfo1155[](maxCount);
-        for (uint i=0; i<maxCount; i++) {
-            nftInfos[i].id = _mintedIds.at(offset*pageMax+i);
-            nftInfos[i].amount = balanceOf(owner, _mintedIds.at(offset*pageMax+i) );
+        for (uint256 i = 0; i < maxCount; i++) {
+            nftInfos[i].id = _mintedIds.at(offset * pageMax + i);
+            nftInfos[i].amount = balanceOf(
+                owner,
+                _mintedIds.at(offset * pageMax + i)
+            );
         }
-
     }
 
     /**
      * @dev function to get the all of the ids.
      */
-    function totalIds() external override view returns ( uint256[] memory ids ) {
-        
-        uint maxCount = _mintedIds.length();
+    function totalIds() external view override returns (uint256[] memory ids) {
+        uint256 maxCount = _mintedIds.length();
         ids = new uint256[](maxCount);
-        for(uint i=0; i<maxCount; i++){
+        for (uint256 i = 0; i < maxCount; i++) {
             ids[i] = _mintedIds.at(i);
         }
         return ids;
     }
-
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # halo-contracts
 
-# bsc main 
+# bsc main  
 # https://bscscan.com/address/0x1f8ffda4d85b9f46e6b05d10fdb1f4e1b814b519#code
 # address:0x1f8ffda4d85b9f46e6b05d10fdb1f4e1b814b519
 
